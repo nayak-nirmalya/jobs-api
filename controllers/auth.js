5e5127d606685c3883177743f9f2625e6023d735
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-17 10:01:44 +0530

    saving hashed pw

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 const User = require("../models/User");
 
 const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json(user);
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,8 +21,15 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please Provide a Password!"],
     minlength: 4,
-    maxlength: 12,
+    select: false,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
