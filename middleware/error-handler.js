8a8afc3d9008b7e0794496b9a68dd5ba601450c3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-17 23:16:08 +0530

    sending custom error for cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -25,6 +25,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = StatusCodes.NOT_ACCEPTABLE;
   }
 
+  if (err.name === "CastError") {
+    customError.msg = `No Item Found with ID: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
