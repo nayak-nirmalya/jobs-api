5c6dd47cfade34dfb6258ca8ecf4ddae1f9f0598
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-17 23:05:07 +0530

    sending custom error for duplicate entry

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,10 +2,24 @@ const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something Unexpected Happended!",
+  };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate Value Entered for ${Object.keys(err.keyValue)
+      .toString()
+      .toUpperCase()} Field`;
+    customError.statusCode = StatusCodes.NOT_ACCEPTABLE;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
