e2b3b38c517339a97dd6b83754e077bdcfd16567
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-17 22:38:10 +0530

    update job by id

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,6 +7,7 @@ const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
+
 const getJob = async (req, res) => {
   const {
     user: { userId },
@@ -22,14 +23,37 @@ const getJob = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
-  res.send("Update Job!");
+  const {
+    body: { company, position, status },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (!company || !position || !status)
+    throw new BadRequestError("Please, Provide Company, Position and Status!");
+
+  const job = await Job.findOneAndUpdate(
+    {
+      _id: jobId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) throw new NotFoundError(`No Job with Job ID: ${jobId}`);
+
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const deleteJob = async (req, res) => {
   res.send("Delete Job!");
 };
